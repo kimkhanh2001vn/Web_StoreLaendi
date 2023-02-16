5f5c43bc624d534dbb4dd7e46655eed8551fca1b
kimkhannh2001vn <ngokhanh858@gmail.com>
2023-02-16 21:17:16 +0700

    first commit

---
+++ LaendiStore.Web/App_Start/BundleConfig.cs
@@ -0,0 +1,25 @@
+﻿using System.Web;
+using System.Web.Optimization;
+
+namespace LaendiStore.Web
+{
+    public class BundleConfig
+    {
+        // For more information on bundling, visit https://go.microsoft.com/fwlink/?LinkId=301862
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+                        "~/assets/js/jquery-3.6.0.min.js"));
+
+            bundles.Add(new ScriptBundle("~/assets/js").Include(
+                      "~/assets/js/plugins.js",
+                      "~/assets/js/main.js"));
+
+            bundles.Add(new StyleBundle("~/assets/css").Include(
+                      "~/assets/css/bootstrap.min.css",
+                      "~/assets/css/plugins.css",
+                      "~/assets/css/style.css",
+                      "~/assets/css/responsive.css"));
+        }
+    }
+}
